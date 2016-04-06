17ef3c4cc7ef92173fd967eba9761ee9449f3a81
RedFantom <redfantom@outlook.com>
2016-04-06 14:17:28 +0200

    Added the 'extra' for Exercise 2

--- Exercise 2 - Odd or Even.py
+++ Exercise 2 - Odd or Even.py
@@ -1,7 +1,10 @@
 print "Please enter a number:"
 number = int(raw_input())
 if(number % 2 == 0):
-    print "Your number was even"
+    if(number % 4 == 0):
+        print "Your number was a multiple of four"
+    else:
+        print "Your number was even"
 elif(number % 2 == 1):
     print "Your number was odd"
 else:
