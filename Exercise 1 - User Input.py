39784d463cb9eb07d5be0acf7ec9a5f9ae9328ea
RedFantom <redfantom@outlook.com>
2016-04-06 14:16:01 +0200

    Completed the first two tasks

---
+++ Exercise 1 - User Input.py
@@ -0,0 +1,5 @@
+print "What is your name?"
+name = raw_input()
+print "How old are you?"
+age = int(raw_input())
+print name, " will turn 100 in ", 100 - age, " years"
---
+++ Exercise 2 - Odd or Even.py
@@ -0,0 +1,9 @@
+print "Please enter a number:"
+number = int(raw_input())
+if(number % 2 == 0):
+    print "Your number was even"
+elif(number % 2 == 1):
+    print "Your number was odd"
+else:
+    print "An error has occurred"
+    
